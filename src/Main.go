246b97749b2ef0b433e31e97cc287ee0c0c25ac3
wujunyu <junyuwu1995@foxmail.com>
2021-01-31 13:29:37 +0800

    'init'

---
+++ src/Main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/sendData", sendData)
+	http.ListenAndServe(":8090", nil)
+}
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func sendData(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
